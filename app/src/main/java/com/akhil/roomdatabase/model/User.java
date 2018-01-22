62d8c012dcad34243e82b196e65f0bfc374fb8cb
akhilsoni06 <soni.akhil06@gmail.com>
2018-01-22 20:48:39 +0530

    updated the add and delete user functionality.

--- app/src/main/java/com/akhil/roomdatabase/model/User.java
+++ app/src/main/java/com/akhil/roomdatabase/model/User.java
@@ -5,7 +5,7 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 
 /**
- * Created by e on 12-01-2018.
+ * Created by Akhil on 12-01-2018.
  */
 @Entity(tableName = "user")
 public class User {
